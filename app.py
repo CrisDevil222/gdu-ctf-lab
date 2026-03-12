e71ba8afc9359a3306039345a1e41f684e61e342
CrisDevil222 <son99539@gmail.com>
2026-03-13 01:21:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ FLAG = os.environ.get("FLAG", "CTF{sql1_1nj3ct10n_1s_fun_and_easy}")
 
 # Init DB
 def init_db():
-    conn = sqlite3.connect("/tmp/users.db")
+    conn = sqlite3.connect("users.db")
     c = conn.cursor()
     c.execute("""CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY,
